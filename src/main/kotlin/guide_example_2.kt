0bb0b7dcb10baba8387306d665dfe7b6454fd7ba
baob <coder@onesandthrees.com>
2017-10-11 14:55:10 +0100

    refactor to HttpHandler

--- src/main/kotlin/guide_example_2.kt
+++ src/main/kotlin/guide_example_2.kt
@@ -1,15 +1,19 @@
 package guide.example._2_adding_the_first_endpoint
 
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.*
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
 fun MyMathServer(port: Int): Http4kServer = MyMathsApp().asServer(Jetty(port))
 
-fun MyMathsApp(): (Request) -> Response {
-    return { _: Request -> Response(OK) }
-}
+fun MyMathsApp(): HttpHandler = routes(
+"/ping" bind GET to { _: Request -> Response(OK) }
+)
 
