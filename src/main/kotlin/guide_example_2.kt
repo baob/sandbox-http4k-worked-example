f6fe9818bd5913cd908246c4b9441e0a19c0f43d
baob <coder@onesandthrees.com>
2017-10-11 15:48:32 +0100

    insert addition functionality

--- src/main/kotlin/guide_example_2.kt
+++ src/main/kotlin/guide_example_2.kt
@@ -5,6 +5,8 @@ import org.http4k.core.Method.*
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.lens.Query
+import org.http4k.lens.int
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Http4kServer
@@ -15,6 +17,9 @@ fun MyMathServer(port: Int): Http4kServer = MyMathsApp().asServer(Jetty(port))
 
 fun MyMathsApp(): HttpHandler = routes(
   "/ping" bind GET to { _: Request -> Response(OK) },
-        "/add" bind GET to { _: Request -> Response(OK) }
+        "/add" bind GET to { request: Request ->
+            val valuesToAdd = Query.int().multi.defaulted("value", listOf()).extract(request)
+            Response(OK).body(valuesToAdd.sum().toString())
+        }
 )
 
