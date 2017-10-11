30e6c4d80c82ff5ca896e800f9a860d92b686ca3
baob <coder@onesandthrees.com>
2017-10-11 15:55:11 +0100

    add CatchLensFailureFilter

--- src/main/kotlin/guide_example_2.kt
+++ src/main/kotlin/guide_example_2.kt
@@ -5,6 +5,8 @@ import org.http4k.core.Method.*
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.filter.ServerFilters
 import org.http4k.lens.Query
 import org.http4k.lens.int
 import org.http4k.routing.bind
@@ -15,11 +17,12 @@ import org.http4k.server.asServer
 
 fun MyMathServer(port: Int): Http4kServer = MyMathsApp().asServer(Jetty(port))
 
-fun MyMathsApp(): HttpHandler = routes(
-  "/ping" bind GET to { _: Request -> Response(OK) },
-        "/add" bind GET to { request: Request ->
-            val valuesToAdd = Query.int().multi.defaulted("value", listOf()).extract(request)
-            Response(OK).body(valuesToAdd.sum().toString())
-        }
+fun MyMathsApp(): HttpHandler = ServerFilters.CatchLensFailure.then(
+        routes(
+                "/ping" bind GET to { _: Request -> Response(OK) },
+                "/add" bind GET to { request: Request ->
+                    val valuesToAdd = Query.int().multi.defaulted("value", listOf()).extract(request)
+                    Response(OK).body(valuesToAdd.sum().toString())
+                }
+        )
 )
-
